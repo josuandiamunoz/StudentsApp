2d1a4532209e345f5106a8361ad6471d3884ed3e
Josu Andia <andiajosu@gmail.com>
2026-05-01 14:36:46 +0200

    Change field from "username" to "email". Call backend endpoint to validate login.

--- frontend/src/controls/views/login/Login.js
+++ frontend/src/controls/views/login/Login.js
@@ -1,13 +1,38 @@
+import  { useState } from "react";
+import axios from "axios";
 import TextBox from "../../common/textbox/TextBox";
 import Button from "../../common/button/Button";
 
 
 function Login(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async () => {
+        try {
+            const response = await axios.post("http://localhost:3001/login", { 
+                email, 
+                password
+            });
+            alert("Login successful.");
+        } catch (error) {
+            alert("Login failed: " + error);
+        }
+    };
     return (
         <div>
-            <TextBox id="username" label="Username" />
-            <TextBox id="password" label="Password" isPassword={true} />
-            <Button onClick={() => console.log("Login button clicked")} text="Login" />
+            <TextBox 
+                id="email" 
+                label="Email" 
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextBox 
+                id="password" 
+                label="Password" 
+                isPassword={true}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button onClick={handleLogin} text="Login" />
         </div>
     );
 }
