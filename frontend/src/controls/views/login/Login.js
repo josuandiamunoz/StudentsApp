1be8c595acdfb410e21454c52b4c4dc490b8e456
Josu Andia <andiajosu@gmail.com>
2026-05-10 12:58:38 +0200

    Communicate login page with backend.

--- frontend/src/controls/views/login/Login.js
+++ frontend/src/controls/views/login/Login.js
@@ -1,20 +1,38 @@
+import axios from 'axios';
+import React, { useState } from 'react';
 import TextBox from "../../common/textbox/TextBox";
 import Button from "../../common/button/Button";
 
 
 function Login(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async () => {
+        try {
+            const response = await axios.post("http://localhost:3001/login", { 
+                email, 
+                password
+            });
+            alert("Login successful.");
+        } catch (error) {
+            alert("Login failed: " + error);
+        }
+    };
     return (
         <div>
             <TextBox 
                 id="email" 
                 label="Email" 
+                onChange={(e) => setEmail(e.target.value)}
             />
             <TextBox 
                 id="password" 
                 label="Password" 
                 isPassword={true}
+                onChange={(e) => setPassword(e.target.value)}
             />
-            <Button text="Login" />
+            <Button onClick={handleLogin} text="Login" />
         </div>
     );
 }
