92c0fde823a947fcf6d8b6dff7f5ed12447533ca
Josu Andia <andiajosu@gmail.com>
2026-05-13 18:17:25 +0200

    PublicRoute implemented, so that authenticated users are redirected to main page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './controls/views/login/Login';
+import PublicRoute from './routes/PublicRoute';
 import ProtectedRoute from './routes/ProtectedRoute';
 import Main from './controls/layout/Main';
 
@@ -7,7 +8,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={
+              <PublicRoute>
+                  <Login />
+              </PublicRoute>
+            }
+          />
           <Route path="/main" element={
                 <ProtectedRoute>
                     <Main />
