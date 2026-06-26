356016b380ed3c7d04d72018cb304a09c66664f9
Josu Andia <andiajosu@gmail.com>
2026-06-26 16:45:46 +0200

    Retrieve students list from backend. Show students list.

--- frontend/src/controls/views/students/Students.js
+++ frontend/src/controls/views/students/Students.js
@@ -1,8 +1,59 @@
 
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Button from "../../common/button/Button";
+
 function Students() {
+    const [students, setStudents] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    const loadStudents = async () => {
+        try{
+            setLoading(true);
+            setError("");
+
+            const token = localStorage.getItem("token");
+            const response = await axios.get("http://localhost:3001/students", {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            setStudents(response.data.data);
+        }
+        catch (error) {
+            setError("Could not load students: " + error.message);
+        }
+        finally{
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        loadStudents();
+    }, []);
+    
     return (
         <div>
-            <h1>This is the students view</h1>
+            <h1>Students list</h1>
+            <Button onClick={loadStudents} text={loading ? 'Loading...' : 'Reload'} />
+            {error && <p style={{ color: "red" }}>{error}</p>}
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {students.map((student) => (
+                        <tr key={student.Identifier}>
+                            <td>{student.Identifier}</td>
+                            <td>{student.Email}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
