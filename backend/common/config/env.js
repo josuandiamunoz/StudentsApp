b7c42485c58ea431106e31f51f6884de52f0e600
Josu Andia <andiajosu@gmail.com>
2026-09-26 16:05:43 +0200

    Include port number in database access configuration, so that connection can be stablished from docker container.

--- backend/common/config/env.js
+++ backend/common/config/env.js
@@ -1,6 +1,7 @@
 const config = {
     db: {
         server: process.env.DB_SERVER,
+        port: Number(process.env.DB_PORT),
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
